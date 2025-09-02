8c32543f5b9fa0742440759245344bbb81bdb861
Nathan <nbel806@aucklanduni.ac.nz>
2025-09-02 17:21:22 +1200

    feat:add-shadcn

---
+++ BookTinderFullStack/app/login/login.tsx
@@ -0,0 +1,3 @@
+export function Login() {
+  return <div>Login 1</div>;
+}
---
+++ BookTinderFullStack/app/routes/login.tsx
@@ -0,0 +1,17 @@
+import { Login } from "~/login/login";
+import type { Route } from "./+types/login";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export function loader({ context }: Route.LoaderArgs) {
+  return { message: context.VALUE_FROM_EXPRESS };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  return <Login />;
+}
