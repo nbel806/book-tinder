995684b104bdbbaab46f6fa5060b17f422cc82d7
Nathan <nbel806@aucklanduni.ac.nz>
2025-09-03 16:43:43 +1200

    feat:login UI

--- BookTinderFullStack/app/login/login.tsx
+++ BookTinderFullStack/app/login/login.tsx
@@ -1,3 +1,79 @@
+import { useState } from "react";
+import { Link } from "react-router";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
+
 export function Login() {
-  return <div>Login 1</div>;
+  function handleLogin(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const password = (event.target as HTMLFormElement).password.value;
+    const email = (event.target as HTMLFormElement).email.value;
+    const form = event.target as HTMLFormElement;
+
+    //Check login info
+    //If correct then redirect to dashboard
+    //form.reset();
+
+    //If incorrect then display error
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-200">
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email and password below to login to your account
+          </CardDescription>
+          <CardAction>
+            <Link to="/signup">
+              <Button variant="link">Sign Up</Button>
+            </Link>
+          </CardAction>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleLogin} id="login-form">
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="example@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+          <Button type="submit" className="w-full" form="login-form">
+            Login
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
--- BookTinderFullStack/app/welcome/welcome.tsx
+++ BookTinderFullStack/app/welcome/welcome.tsx
@@ -1,10 +1,13 @@
+import { Link, redirect } from "react-router";
 import { Button } from "~/components/ui/button";
 
 export function Welcome() {
   return (
     <div>
       <h1>Welcome to Book Tinder</h1>
-      <Button>Login</Button>
+      <Link to="/login">
+        <Button>Login</Button>
+      </Link>
     </div>
   );
 }
