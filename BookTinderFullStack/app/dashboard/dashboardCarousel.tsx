954468b13b72a1899f5292e33105a0f89c6448d9
Nathan <nbel806@aucklanduni.ac.nz>
2025-09-04 17:36:41 +1200

    feat:liked and disliked

--- BookTinderFullStack/app/dashboard/dashboardCarousel.tsx
+++ BookTinderFullStack/app/dashboard/dashboardCarousel.tsx
@@ -2,14 +2,14 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "~/components/ui/carousel";
 import { type CarouselApi } from "~/components/ui/carousel";
 import BookCard from "./bookCard";
 import { useEffect, useState, useCallback, useRef } from "react";
 import type { BookInfo } from "~/lib/types";
 import { loadBooks } from "./dashboardHelper";
+import { Button } from "~/components/ui/button";
+import { Check, X } from "lucide-react";
 
 export default function DashboardCarousel() {
   const [books, setBooks] = useState<BookInfo[]>([]);
@@ -46,17 +46,33 @@ export default function DashboardCarousel() {
     };
   }, [api, loadMore]);
 
+  const handleNext = () => api?.scrollNext();
+
   return (
-    <Carousel className="w-full max-w-3xl" setApi={setApi}>
-      <CarouselContent>
-        {books.map((book, i) => (
-          <CarouselItem key={i}>
-            <BookCard {...book} />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <div className="w-full max-w-3xl mx-auto flex flex-col items-center gap-6">
+      <Carousel className="w-full max-w-3xl" setApi={setApi}>
+        <CarouselContent>
+          {books.map((book, i) => (
+            <CarouselItem key={i}>
+              <BookCard {...book} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+      <div className="flex gap-6 mt-2">
+        <Button
+          onClick={handleNext}
+          className="size-16 bg-blue-100 hover:bg-blue-200 text-slate-800 p-2 rounded-full shadow-lg transition transform hover:scale-105"
+        >
+          <X />
+        </Button>
+        <Button
+          className="size-16 bg-pink-100 hover:bg-pink-200 text-slate-800 p-2 rounded-full shadow-lg transition transform hover:scale-105"
+          onClick={handleNext}
+        >
+          <Check />
+        </Button>
+      </div>
+    </div>
   );
 }
